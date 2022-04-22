c7950d2d6ffe0c7a740afd96279e42f6cea9c1af
Minji Kwon <melaniemkwon@gmail.com>
2022-04-22 12:56:14 -0700

    Set up navigation with React Router using Link tag

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 const PageOne = () => {
-  return <div>page 1</div>
+  return (
+    <div>
+      page 1
+      <Link to="/pagetwo">Go to Page Two</Link>
+    </div>
+  );
 };
 const PageTwo = () => {
-  return <div>page 2<button>Click me!</button></div>
+  return (
+    <div>
+      page 2<button>Click me!</button>
+      <Link to="/">Go to Page One</Link>
+    </div>
+  );
 };
 
 const App = () => {
