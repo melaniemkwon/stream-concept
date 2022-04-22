d99f30cadb609e7232107f87eb6efc0f64eb83e7
Minji Kwon <melaniemkwon@gmail.com>
2022-04-22 12:25:55 -0700

    Install React-Router-Dom v5. Set up dummmy example for testing.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const PageOne = () => {
+  return <div>page 1</div>
+};
+const PageTwo = () => {
+  return <div>page 2<button>Click me!</button></div>
+};
 
 const App = () => {
-  return <div>App</div>
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={PageOne} />
+          <Route path="/pagetwo" component={PageTwo} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
