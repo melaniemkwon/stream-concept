1f2c683de895b69ea29bf14c77eb24ca1defc484
Minji Kwon <melaniemkwon@gmail.com>
2022-04-27 11:04:51 -0700

    comment cleanup

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -3,9 +3,7 @@ import { connect } from 'react-redux';
 import { signIn, signOut } from '../actions'
 
 class GoogleAuth extends React.Component {
-  // get rid of component level state for redux
-  // state = { isSignedIn: null };
-
+  
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
       window.gapi.client.init({
@@ -13,7 +11,6 @@ class GoogleAuth extends React.Component {
         scope: 'email'
       }).then(() => {
         this.auth = window.gapi.auth2.getAuthInstance();
-        // this.setState({ isSignedIn: this.auth.isSignedIn.get() });
         this.onAuthChange(this.auth.isSignedIn.get());
         this.auth.isSignedIn.listen(this.onAuthChange);
       });
@@ -37,10 +34,8 @@ class GoogleAuth extends React.Component {
   };
 
   renderAuthButton() {
-    // if (this.state.isSignedIn === null) {
     if (this.props.isSignedIn === null) {
       return null;
-    // } else if (this.state.isSignedIn) {
     } else if (this.props.isSignedIn) {
       return (
         <button className="ui red google button" onClick={this.onSignOut}>
