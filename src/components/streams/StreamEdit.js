1b05be4bdc485a4688b062bd7e1d016aaa864874
Minji Kwon <melaniemkwon@gmail.com>
2022-05-05 09:44:56 -0700

    pick out form values we care about, leave out id and userid

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchStream, editStream } from '../../actions';
@@ -23,7 +24,7 @@ class StreamEdit extends React.Component {
         <h3>Edit a Stream</h3>
         <StreamForm 
           // initialValues={{ title: 'edit me', description: 'changemepls' }}
-          initialValues={this.props.stream}
+          initialValues={_.pick(this.props.stream, 'title', 'description')}
           onSubmit={this.onSubmit} 
         />
       </div>
