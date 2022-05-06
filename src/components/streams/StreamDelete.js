6aa1493a1c6b9df1b3b6fef0e491aac7c73fef0a
Minji Kwon <melaniemkwon@gmail.com>
2022-05-06 08:17:40 -0700

    Make modal component reusable. Still need to address button styling issue regarding extra wrapping div

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -2,10 +2,21 @@ import React from 'react';
 import Modal from '../modal';
 
 const StreamDelete = () => {
+  const actions = (
+    <div>
+      <button className="ui negative button">Delete</button>
+      <button className="ui button">Cancel</button>
+    </div>
+  );
+
   return (
     <div>
       StreamDelete
-      <Modal />
+      <Modal 
+        title="Delete Stream"
+        content="Are you sure you want to delete this stream?"
+        actions={actions}
+      />
     </div>
   );
 };
