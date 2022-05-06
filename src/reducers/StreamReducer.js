1b67be6582a8b4002ed93407be4b603f56ba0578
Minji Kwon <melaniemkwon@gmail.com>
2022-05-06 10:44:41 -0700

    Deal with FetchStream typos

--- src/reducers/StreamReducer.js
+++ src/reducers/StreamReducer.js
@@ -12,7 +12,7 @@ export default (state = {}, action) => {
     case FETCH_STREAMS:
       return { ...state, ..._.mapKeys(action.payload, 'id') };
     case FETCH_STREAM: 
-      return { ...state, [action.paylaod.id]: action.payload };
+      return { ...state, [action.payload.id]: action.payload };
     case CREATE_STREAM: 
       return { ...state, [action.payload.id]: action.payload };
     case EDIT_STREAM: 
